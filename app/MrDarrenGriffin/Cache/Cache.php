12914ddcef649212c1ce8f37440cca112ad0560c
Darren Griffin <darren.griffin@live.co.uk>
2016-02-22 15:32:00 +0000

    Added Cache Controller

---
+++ app/MrDarrenGriffin/Cache/Cache.php
@@ -0,0 +1,31 @@
+<?php
+namespace MrDarrenGriffin\Cache;
+Class Cache{
+
+  public $cacheDirectory = '';
+  public $controller = '';
+
+  public function __construct($cacheDirectory, $controller){
+    $this->cacheDirectory = rtrim($cacheDirectory,'/').'/';
+    $this->controller = rtrim($controller,'/').'/';
+    if(!is_dir($this->cacheDirectory.$this->controller)){mkdir($this->cacheDirectory.$this->controller,0777,true);}
+
+  }
+
+  public function store($url,$filename){
+    return copy($url,$this->cacheDirectory.$this->controller.$filename);
+  }
+
+  public function retrieve($filename){
+    return $this->cacheDirectory.$this->controller.$filename;
+  }
+
+  public function isInCache($filename){
+   return file_exists($this->cacheDirectory.$this->controller.$filename);
+  }
+
+  public function remove($filename){
+    return delete($this->cacheDirectory.$this->controller.$filename);
+  }
+
+}
